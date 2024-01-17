49f9d3d557ba522614cf489a4c7da55bc2e108da
sydsedloff <ssedloff@knights.ucf.edu>
2024-01-17 16:36:11 -0500

    navigation

--- FoodVenture/App.js
+++ FoodVenture/App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  NavigationContainer,
+  useFocusEffect,
+  useRoute,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="WelcomeScreen">
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function handlePress() {
+  navigation.navigate(LoginScreen);
+}
+function WelcomeScreen({ navigation }) {
+  return (
+    <View style={[styles.container]}>
+      <Text>Welcome</Text>
+      <Text>to FoodVenture</Text>
+      <TouchableOpacity onPress={()=navigation.navigate(LoginScreen)}>
+        Log In
+      </TouchableOpacity>
+      <Button>Sign Up</Button>
+    </View>
+  );
+}
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={[styles.container]}>
+      <Text>This is the Login screen</Text>
+    </View>
+  );
+}
+
+function SignUpScreen({ navigation }) {
+  return (
+    <View style={[styles.container]}>
+      <Text>This is the Sign Up screen</Text>
     </View>
   );
 }
@@ -13,8 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
