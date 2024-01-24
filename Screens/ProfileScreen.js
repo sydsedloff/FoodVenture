59b485b4df07c90237c99ca5683367dff0dea0e0
sydsedloff <ssedloff@knights.ucf.edu>
2024-01-24 12:37:25 -0500

    EditProfile Screen content

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -19,26 +19,50 @@ export default function ProfileScreen({ navigation }) {
         style={styles.logo}
         source={require("../assets/FViconYellow.png")}
       />
-      <Text>USERNAME</Text>
+      <Text style={[styles.h3.b]}>USERNAME</Text>
       <Text>FIRSTNAME LASTNAME</Text>
       <Pressable onPress={() => navigation.navigate(EditProfileScreen)}>
+        <Image
+          source={require("../assets/profileIcon.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text>Edit Profile</Text>
       </Pressable>
       <Pressable
         onPress={() => navigation.navigate(EditDietaryRestrictionsScreen)}
       >
+        <Image
+          source={require("../assets/forkKnifeRed.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text>Edit Dietary Restrictions</Text>
       </Pressable>
       <Pressable onPress={() => navigation.navigate(SavedScreen)}>
+        <Image
+          source={require("../assets/saveFilled.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text>Saved</Text>
       </Pressable>
       <Pressable>
+        <Image
+          source={require("../assets/noti.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text>Notifications</Text>
       </Pressable>
       <Pressable>
+        <Image
+          source={require("../assets/settingGear.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text>Setting</Text>
       </Pressable>
       <Pressable>
+        <Image
+          source={require("../assets/logout.png")}
+          style={[styles.icon]}
+        ></Image>
         <Text onPress={() => navigation.navigate(WelcomeScreen)}>Log out</Text>
       </Pressable>
       {/* keyExtractor={(item) => item.id} */}
