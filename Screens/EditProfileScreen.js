83298a9fbb3ebf383b44a5bf5fec0e53e618fe24
sydsedloff <ssedloff@knights.ucf.edu>
2024-02-24 14:19:06 -0500

    prefilled form edit profile page

--- Screens/EditProfileScreen.js
+++ Screens/EditProfileScreen.js
@@ -6,10 +6,35 @@ import {
   View,
   SafeAreaView,
 } from "react-native";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "../styles";
 import ProfileScreen from "./ProfileScreen";
 
 export default function EditProfileScreen({ navigation }) {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    try {
+      const userEmail = await AsyncStorage.getItem("userEmail");
+      const response = await fetch(
+        `http://localhost:3000/api/userData/${userEmail}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      } else {
+        console.error("Failed to fetch user data");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+
   return (
     <SafeAreaView>
       <SafeAreaView style={[styles.headerContainer]}>
@@ -24,41 +49,61 @@ export default function EditProfileScreen({ navigation }) {
         <Text style={[styles.pageHeaders, styles.bottomMargins]}>
           Edit Profile
         </Text>
-        <Image
-          style={[styles.logoR]}
-          source={{ uri: "https://placehold.co/100x100/" }}
-        />
-
+        {userData && ( // Render the Image component conditionally when userData is not null
+          <Image
+            style={[styles.logoR]}
+            source={{ uri: userData.profilePicture }}
+          />
+        )}
         <View style={[styles.textInputContainer, styles.width80]}>
           <View>
             <Text style={[styles.signa28]}>Name</Text>
-            <TextInput placeholder="Name" style={styles.thinInput} />
+            <TextInput
+              placeholder="Name"
+              style={styles.thinInput}
+              value={userData ? userData.fullName : ""}
+            />
           </View>
           <View>
             <Text style={[styles.signa28]}>Username</Text>
-            <TextInput placeholder="Username" style={styles.thinInput} />
+            <TextInput
+              placeholder="Username"
+              style={styles.thinInput}
+              value={userData ? userData.username : ""}
+            />
           </View>
-          <View>
+          {/* <View>
             <Text style={[styles.signa28]}>Dietary Restrictions</Text>
             <TextInput
               placeholder="Dietary Restrictions"
               style={styles.thinInput}
+              value={userData ? userData.dietaryRestrictions : ""}
             />
-          </View>
+          </View> */}
           <View>
             <Text style={[styles.signa28]}>Email</Text>
             <TextInput
-              placeholder="hello@foodventure.com"
+              placeholder="Email"
               style={styles.thinInput}
+              value={userData ? userData.email : ""}
             />
           </View>
           <View>
             <Text style={[styles.signa28]}>Phone Number</Text>
-            <TextInput placeholder="123-456-7890" style={styles.thinInput} />
+            <TextInput
+              placeholder="123-456-7890"
+              style={styles.thinInput}
+              value={userData ? userData.phoneNumber : ""}
+            />
           </View>
           <View>
             <Text style={[styles.signa28]}>Password</Text>
-            <TextInput placeholder="Password" style={styles.thinInput} />
+            <TextInput
+              placeholder="Password"
+              style={styles.thinInput}
+              secureTextEntry={true}
+              value={userData ? userData.password : ""}
+            />
           </View>
 
           <Pressable style={[styles.buttonLarge.r]}>
