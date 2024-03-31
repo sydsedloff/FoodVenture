f5cfd4d6d3b4373f8994c3bd84a740172f0489f1
sydsedloff <ssedloff@knights.ucf.edu>
2024-03-31 14:48:19 -0400

    save single restaurants

--- Components/RestaurantsComponent.js
+++ Components/RestaurantsComponent.js
@@ -1,11 +1,6 @@
+// RestaurantsComponent.js
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  Pressable,
-  Linking,
-} from "react-native";
+import { View, Text, Image, Pressable, Linking } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "../styles";
 import RatingImage from "./RatingImageComponent";
@@ -18,11 +13,10 @@ const Restaurants = ({
   website,
   navigation,
   star_rating,
-  restaurantId, 
+  restaurantId,
 }) => {
   const [saved, setSaved] = useState(false);
 
-  // Define restaurantData object
   const restaurantData = {
     name,
     image,
@@ -30,7 +24,38 @@ const Restaurants = ({
     description,
     website,
     star_rating,
+    restaurantId,
   };
+
+  async function saveRestaurantDB(restaurantData) {
+    try {
+      const userEmail = await AsyncStorage.getItem("userEmail");
+      console.log("user email is: " + userEmail);
+
+      // Update the database with the new restaurant data
+      const response = await fetch(
+        `http://localhost:3000/api/saveRestaurant/${userEmail}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userEmail: userEmail,
+            savedRestaurant: restaurantData,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to save restaurant");
+      }
+
+      console.log("Restaurant saved successfully");
+    } catch (error) {
+      console.error("Error saving restaurant:", error);
+    }
+  }
 
   const saveRestaurant = async (restaurantData) => {
     try {
@@ -44,19 +69,35 @@ const Restaurants = ({
 
   const handlePress = () => {
     saveRestaurant(restaurantData);
-    navigation.navigate("RestaurantScreen", { params: {restaurantId} });
+    navigation.navigate("RestaurantScreen", { params: { restaurantId } });
   };
 
-
-  const toggleSave = () => {
+  const toggleSave = async () => {
     setSaved(!saved);
+    const { name, address, description, website, star_rating, restaurantId } =
+      restaurantData;
+    const dataToSave = {
+      name,
+      address,
+      description,
+      website,
+      star_rating,
+      restaurantId,
+    };
+    await saveRestaurantDB(dataToSave);
   };
 
   const placeholderImage = require("../assets/FoodVenturePlaceholder.png");
 
   return (
     <View
-      style={[styles.container, styles.bottomMargins, styles.alignItemsLeft, styles.width80, styles.alignSelfCenter]}
+      style={[
+        styles.container,
+        styles.bottomMargins,
+        styles.alignItemsLeft,
+        styles.width80,
+        styles.alignSelfCenter,
+      ]}
     >
       <Pressable onPress={toggleSave}>
         <View style={[styles.horizontalAlign, styles.justifySpaceBetween]}>
@@ -74,10 +115,19 @@ const Restaurants = ({
         </View>
         <RatingImage star_rating={star_rating} />
       </Pressable>
-      <Pressable onPress={handlePress}>
+      <Pressable
+        onPress={() =>
+          navigation.navigate("RestaurantScreen", { restaurantId })
+        }
+      >
         <Image
           source={image ? { uri: image } : placeholderImage}
-          style={[styles.image, styles.lessBottomMargins, styles.alignSelfCenter, styles.width100]}
+          style={[
+            styles.image,
+            styles.lessBottomMargins,
+            styles.alignSelfCenter,
+            styles.width100,
+          ]}
         />
       </Pressable>
       <Text style={[styles.merri19Bold, styles.lessBottomMargins]}>
@@ -89,9 +139,7 @@ const Restaurants = ({
       <Pressable onPress={() => Linking.openURL(website)}>
         <Text style={[styles.link, styles.bottomMargins]}>Restaurant Link</Text>
       </Pressable>
-      <Pressable onPress={handlePress}>
-        {/* Will need to grab key for restaurant screen */}
-      </Pressable>
+      <Pressable onPress={handlePress}></Pressable>
       <View style={[styles.contentSeperatorContainer]}>
         <View style={[styles.line, styles.bottomMargins]} />
       </View>
